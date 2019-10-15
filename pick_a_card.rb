c33f514f4285a95db6088c45e76102124e5241f1
MikeP <to4my@ya.ru>
2019-10-15 14:27:21 +0300

    Масти заменены символами

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -5,6 +5,6 @@ gets
 
 values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
-suits = ["Diamonds", "Hearts", "Clubs", "Spades"]
+suits = %w(♦ ♠ ♥ ♣) # ["Diamonds", "Hearts", "Clubs", "Spades"]
 
-puts "Ваша карта: #{values.sample} of #{suits.sample}"
+puts "Ваша карта: #{values.sample}#{suits.sample}"
